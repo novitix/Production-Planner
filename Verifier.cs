fe448325de42c907cef5a30bd5b9740c846ceeb1
Adam Ye <a2d0a0m2@gmail.com>
2020-07-12 21:57:36 +1000

    fixed previous commit - forgot to add verifiere file

---
+++ Verifier.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+
+namespace Production_Planner
+{
+    static class Verifier
+    {
+        public static bool HasIllegalChars(bool allowDecimal, TextCompositionEventArgs e)
+        {
+
+            Regex reg;
+            if (allowDecimal)
+            {
+                reg = new Regex("[^0-9.]+");
+            }
+            else
+            {
+                reg = new Regex("[^0-9]+");
+            }
+            return reg.IsMatch(e.Text);
+        }
+    }
+}
